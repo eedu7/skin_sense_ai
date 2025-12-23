87bcc0f18807114379c92691fc46b6a3748e5d43
eedu7 <mueedahmad067@gmail.com>
2025-12-23 20:52:14 +0500

    Initialized the project

---
+++ src/integrations/tanstack-react-query/index.tsx
@@ -0,0 +1,12 @@
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+export const TanstackReactQuery = ({
+    children,
+}: Readonly<{ children: React.ReactNode }>) => {
+    const queryClient = new QueryClient();
+    return (
+        <QueryClientProvider client={queryClient}>
+            {children}
+        </QueryClientProvider>
+    );
+};
